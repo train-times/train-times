ae9bbeea1953165234d1d940e275cdf98ef4a0c9
Roman <dev@rman.dev>
2026-07-13 18:05:47 +0100

    feat!: export more utils, rename `getJourneysData`
    renamed `getJourneyInfo` to `getJourneysData`

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
-import { normalizeTrainServicesData } from "./lib/normalize";
-import type { TrainJourney } from "./lib/normalize";
 import { getDeparturesArrivalsData } from "./api";
 import type { LiveDeparturesApiOptions } from "./api";
+import { normalizeTrainServicesData } from "./lib/normalize";
+import type { TrainJourney } from "./lib/normalize";
 
-async function getJourneyInfo(
+async function getJourneysData(
   options: LiveDeparturesApiOptions,
 ): Promise<TrainJourney[]> {
   const data = await getDeparturesArrivalsData(options);
   return normalizeTrainServicesData(data, options.to);
 }
 
-export { getJourneyInfo };
+export {
+  getJourneysData,
+  getDeparturesArrivalsData,
+  normalizeTrainServicesData,
+};
+
+export type { LiveDeparturesApiOptions };
+
+export type { LiveDeparturesArrivalsApiResponse } from "./api/types";
 
 export type * from "./lib/types";
